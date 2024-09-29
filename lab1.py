d9b6531cb25db0bc6e631d8ddacb60d3d51ded15
Mantas <mantas.ziln@gmail.com>
2024-09-29 22:39:33 +0300

    all funcs degraph-ified

--- lab1.py
+++ lab1.py
@@ -4,17 +4,15 @@ import math
 
 ##########################################################
 
-def funkcija(x): 
+def funkcija(x, a = 2, b = 1): 
     return ((x**2 - a)**2/b) - 1
 
-def funkcijaD(x):
+def funkcijaD(x, a = 2, b = 1): #pirmoji išvestinė
     return 4 *x *(x**2 - a)/b
 
-def funkcijaDD(x):
+def funkcijaDD(x, a = 2, b = 1): #antroji išvestinė
     return 4*(3*x**2 - a)/b
 
-a = 2
-b = 1
 
 intervalas = (0, 10)
 e = 0.0001
@@ -41,8 +39,8 @@ def IntervaloDalijimasPusiau(fun, prad, pab, min, tikslof = 0, iter = 0):
             fXm = fX1
         else:
             fX2 = fun(X2)
+            tikslof += 1      #############
             if(fX2 < fXm):
-                tikslof += 1      #############
                 # plt.fill_between([l, Xm], [-5,-5], y2 = max(Ys) , color='red', alpha=0.3)
                 # plt.scatter([Xm], [fun(Xm)], color='red')
                 # plt.text(Xm, fun(Xm), f'{taskas}', fontsize=10, verticalalignment='bottom', horizontalalignment='right')
@@ -89,60 +87,74 @@ def AuksinisPjuvis(fun, prad, pab, min):
     X1 = pab - T * L
     X2 = prad + T * L
 
-    def vidus(l, X1, X2, r, taskas = 1):
-        if(fun(X2) < fun(X1)):
-            plt.fill_between([l, X1], [-5,-5], y2 = max(Ys) , color='red', alpha=0.3)
-            plt.scatter([X1], [fun(X1)], color='red')
-            plt.text(X1, fun(X1), f'{taskas}', fontsize=10, verticalalignment='bottom', horizontalalignment='right')
-            taskas += 1
+    def vidus(l, X1, X2, r, tikslof = 0, iter = 1, fX1 = None, fX2 = None): #################
+        if(fX1 is None):
+            fX1 = fun(X1)
+            tikslof += 1 #################
+        if(fX2 is None):
+            fX2 = fun(X2)
+            tikslof += 1 #################
+        
+        if(fX2 < fX1):
+            # plt.fill_between([l, X1], [-5,-5], y2 = max(Ys) , color='red', alpha=0.3)
+            # plt.scatter([X1], [fun(X1)], color='red')
+            # plt.text(X1, fun(X1), f'{taskas}', fontsize=10, verticalalignment='bottom', horizontalalignment='right')
+            # taskas += 1
             l = X1
             X1 = X2
+            fX1 = fX2  #################
+            fX2 = None #################
             L = r - l
             X2 = l + T * L
         else:
-            plt.fill_between([r, X2], [-5,-5], y2 = max(Ys) , color='red', alpha=0.3)
-            plt.scatter([X2], [fun(X2)], color='red')
-            plt.text(X2, fun(X2), f'{taskas}', fontsize=10, verticalalignment='bottom', horizontalalignment='right')
-            taskas += 1
+            # plt.fill_between([r, X2], [-5,-5], y2 = max(Ys) , color='red', alpha=0.3)
+            # plt.scatter([X2], [fun(X2)], color='red')
+            # plt.text(X2, fun(X2), f'{taskas}', fontsize=10, verticalalignment='bottom', horizontalalignment='right')
+            # taskas += 1
             r = X2
             L = r - l
             X2 = X1
+            fX2 = fX1
+            fX1 = None
             X1 = r - T * L
-
+        print(f"{iter} {tikslof} {(l,r)} {L}")    ##############################
         if(L < min):
             return (l, r)
         else:
-            return vidus(l, X1, X2, r, taskas)
+            return vidus(l, X1, X2, r, tikslof, iter + 1, fX1, fX2) #############
 
-    Xs = np.linspace(prad, pab, num = 1000)
-    Ys = [fun(y) for y in Xs]
-    fig, ax = plt.subplots() 
-    ax.plot(Xs, Ys)
+    # Xs = np.linspace(prad, pab, num = 1000)
+    # Ys = [fun(y) for y in Xs]
+    # fig, ax = plt.subplots() 
+    # ax.plot(Xs, Ys)
     rezultatas = vidus(prad, X1, X2, pab)
-    plt.show()
+    # plt.show()
     return rezultatas    
 
-def NiutonoMetodas(fun, funD, funDD, prad, pab, min, X0):
-    def vidus(X0, taskas = 1):
+def NiutonoMetodas(funD, funDD, min, X0):  #######################
+    def vidus(X0, iter = 0, tikslof = 0):
         Xn = X0 - funD(X0)/funDD(X0)
-        plt.scatter([Xn], [fun(Xn)], color='red')
-        plt.text(Xn, fun(Xn), f'{taskas}', fontsize=10, verticalalignment='bottom', horizontalalignment='right')
-        taskas += 1
+        tikslof += 2 ##############################
+        # plt.scatter([Xn], [fun(Xn)], color='red')
+        # plt.text(Xn, fun(Xn), f'{taskas}', fontsize=10, verticalalignment='bottom', horizontalalignment='right')
+        # taskas += 1
+        iter += 1 ####################################
+        print(f"{iter} {tikslof} {(X0,Xn)} {X0 - Xn}")    ##############################
         if(abs(X0 - Xn) < min):
             return Xn
         else:
-            return vidus(Xn, taskas)
+            return vidus(Xn, iter, tikslof)
      
-    Xs = np.linspace(prad, pab, num = 10000)
-    Ys = [fun(y) for y in Xs]
-    fig, ax = plt.subplots() 
-    ax.plot(Xs, Ys)
-    plt.scatter([X0], [fun(X0)], color='red')
-    plt.text(X0, fun(X0), '0', fontsize=10, verticalalignment='bottom', horizontalalignment='right')
+    # Xs = np.linspace(prad, pab, num = 10000)
+    # Ys = [fun(y) for y in Xs]
+    # fig, ax = plt.subplots() 
+    # ax.plot(Xs, Ys)
+    # plt.scatter([X0], [fun(X0)], color='red')
+    # plt.text(X0, fun(X0), '0', fontsize=10, verticalalignment='bottom', horizontalalignment='right')
     rezultatas = vidus(X0)
-    plt.show()
+    # plt.show()
     return rezultatas
 
-IntervaloDalijimasPusiau(funkcija, intervalas[0], intervalas[1], e)
+#IntervaloDalijimasPusiau(funkcija, intervalas[0], intervalas[1], e)
 # AuksinisPjuvis(funkcija, intervalas[0], intervalas[1], e)
-# NiutonoMetodas(funkcija, funkcijaD, funkcijaDD, intervalas[0], intervalas[1], e, 5)
+NiutonoMetodas(funkcijaD, funkcijaDD, e, 5)
